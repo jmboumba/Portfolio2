f9d8b24165dc24d575efa56a6ed8e72d6c400e5e
MBOUMBA MOUSSAVOU Junior Melcki <juniormboumba98@gmail.com>
2024-11-02 20:01:42 +0100

    Modification du Header

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+
+function Header(){
+    return(
+        <div>
+            <nav>
+                <center>
+                <ul>
+                    <li><a href="">ABOUT</a></li>
+                    <li><a href="">WORK</a></li>
+                    <li><a href="">SERVICES</a></li>
+                    <li><a href="">EXPERIENCE</a></li>
+                    <li><a href="">BLOG </a></li>
+                    <li><a href="">CONTACT</a></li>
+                </ul>
+                </center>
+            </nav>
+            <hr></hr>
+        </div>
+    )
+}
+
+export default Header;
